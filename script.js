3aed8c54f291132d120b9811284d39f8ab6688a7
joaopassone <joao.passone@gmail.com>
2022-09-04 13:00:29 -0300

    Entrega do Requisito 4

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const rgbColorText = document.getElementById('rgb-color');
+const balls = document.getElementsByClassName('ball');
+
+function colorGenerator() {
+  const red = Math.floor(Math.random() * 255) + 1;
+  const green = Math.floor(Math.random() * 255) + 1;
+  const blue = Math.floor(Math.random() * 255) + 1;
+
+  return `rgb(${red}, ${green} , ${blue})`;
+}
+
+function colorBalls() {
+  for (let index = 0; index < balls.length; index += 1) {
+    balls[index].style.backgroundColor = colorGenerator();
+  }
+}
+
+colorBalls();
